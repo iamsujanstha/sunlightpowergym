a0d2c93e95c7a4afb048e5250f50b524b0cd5af1
Sujan Shrestha <sujan.shrestha2@infodev.com.np>
2024-06-07 19:12:51 +0545

    feat: add mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
+'use client';
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { MdMenu } from 'react-icons/md';
 
 import MobileNav from '@/components/MobileNav';
 import Nav from '@/components/Nav';
 
 const Header = () => {
+  const [isHeaderActive, setHeaderActive] = useState(false);
+  const [isOpenNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHeaderActive(window.scrollY > 50);
+    };
+
+    //add scroll event
+    window.addEventListener('scroll', handleScroll);
+
+    //clear scroll event
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed left-0 right-0 w-full bg-white h-[90px]">
-      <div className="container mx-auto">
+    <header
+      className={clsx(
+        isHeaderActive ? 'h-[100px]' : 'h-[124px]',
+        'fixed flex  w-full bg-primary-200 transition-all z-20',
+      )}
+    >
+      <div className="container mx-aut w-full flex items-center justify-between">
+        <Link href="">
+          <Image src={'/assets/img/logo.png'} width={110} height={55} alt="" />
+        </Link>
         {/* mobile nav - hidden on large devices */}
-        <MobileNav />
+        <MobileNav
+          containerStyles={clsx(
+            isHeaderActive ? 'top-[90px]' : 'top-[124px]',
+            isOpenNav
+              ? 'max-h-max pt-8 pb-10 border-t border-white/10'
+              : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0',
+            'xl:hidden text-white flex flex-col text-center gap-4 text-white fixed bg-primary-200 w-full left-0 top-[124px] text-base uppercase font-medium transition-all',
+          )}
+        />
         {/* destop nav - hidden on small devices */}
-        <Nav containerStyles="flex gap-4 hidden xl:flex" />
+        <Nav containerStyles="flex gap-4 hidden xl:flex text-base uppercase" />
+        {/* hide open menu button */}
+        <div>
+          <button className="text-white xl:hidden" onClick={() => setOpenNav(!isOpenNav)}>
+            <MdMenu className="text-4xl" />
+          </button>
+        </div>
       </div>
     </header>
   );
